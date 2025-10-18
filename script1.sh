4b2756e36709510f7fb61fc263d53827d758be6b
yogesh <myogesh@gmail.com>
2025-10-18 22:09:13 +0530

    script1

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "enter your username"
+
+read -m username
+
+echo "your username is : $username"
+
+echo "enter your password"
+
+read -m password
+
+echo "your password is : $password"
